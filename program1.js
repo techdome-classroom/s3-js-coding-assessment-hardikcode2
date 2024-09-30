4bbe0a255bed69dadc598cc0073ba0b4e263da75
hardikcode2 <moneybro02@gmail.com>
2024-09-30 15:15:14 +0530

    "Implemented isValid function for validating parentheses and romanToInt function for converting Roman numerals to integers"

--- program1.js
+++ program1.js
@@ -3,7 +3,22 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    
+    const stack = [];
+    const map = {
+        '(': ')',
+        '{': '}',
+        '[': ']'
+    };
+    for (let char of s) {
+        if (map[char]) {
+            stack.push(char);
+        } else {
+            if (stack.length === 0 || map[stack.pop()] !== char) {
+                return false;
+            }
+        }
+    }
+    return stack.length === 0;
 };
 
 module.exports = { isValid };
